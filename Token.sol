8df5a53923de00d8442d86a7e38fe90c47fc5f10
savelunaclub <105540463+savelunaclub@users.noreply.github.com>
2022-05-16 09:20:16 +0700

    update
    update

--- Token.sol
+++ Token.sol
@@ -10,6 +10,7 @@ contract Token is Ownable, ERC20,ReentrancyGuard {
     using SafeMath for uint256;
     address public growthFundAddress;
     uint256 public feeRate = 10;
+    uint256 public swapTokensAtAmount = 10*6*10*18;
     IUniswapV2Router02 public uniswapV2Router;
     address public uniswapV2Pair;
     mapping (address => bool) private _isExcludedFromFees;
@@ -20,6 +21,8 @@ contract Token is Ownable, ERC20,ReentrancyGuard {
     uint256 public blacklistAmount;
     event ExcludeFromFees(address indexed account, bool isExcluded);
     event SwapAndLiquify(uint256 tokensSwapped, uint256 ethReceived, uint256 tokensIntoLiqudity);
+    bool swapping = false;
+    
     constructor() ERC20("Save Luna Token", "SLN") {
         growthFundAddress = _msgSender();
         uniswapV2Router = IUniswapV2Router02(
@@ -35,19 +38,8 @@ contract Token is Ownable, ERC20,ReentrancyGuard {
     function setGrowthFundAddress(address _address) public onlyOwner {
         growthFundAddress = _address;
     }
-    
-    function swapTokenForGrowthFund() public nonReentrant {
-        uint256 contractTokenBalance = balanceOf(address(this));
-        address[] memory path = new address[](2);
-        path[0] = address(this);
-        path[1] = uniswapV2Router.WETH();
-        uniswapV2Router.swapExactTokensForETHSupportingFeeOnTransferTokens(
-            contractTokenBalance,
-            0,
-            path,
-            growthFundAddress,
-            block.timestamp
-        );
+    function setAmountToSwap(uint256 _amount) public onlyOwner{
+        swapTokensAtAmount = _amount;
     }
     function excludeFromFees(address account, bool excluded) public onlyOwner {
         require(_isExcludedFromFees[account] != excluded, "Account is already 'excluded'");
@@ -59,7 +51,7 @@ contract Token is Ownable, ERC20,ReentrancyGuard {
     }
      function setBlacklists(address _bots) external onlyOwner {
         require(!bots[_bots]);
-        require(_bots!=uniswapV2Pair,"pair address can not be pair");
+        require(_bots!=uniswapV2Pair,"bot address can not be pair");
         bots[_bots] = true;
     }
 
@@ -71,17 +63,29 @@ contract Token is Ownable, ERC20,ReentrancyGuard {
         if (blacklistEnabled &&(amount > blacklistAmount || bots[sender] )) {
             revert("You're bot");
         }
-        if (sender != address(this) && recipient != address(this) && !_isExcludedFromFees[sender] && !_isExcludedFromFees[recipient]) {
+        if(!swapping && sender!=uniswapV2Pair){
+            swapFeeToken();
+        }
+        if (!swapping && sender != address(this) && recipient != address(this) && !_isExcludedFromFees[sender] && !_isExcludedFromFees[recipient]) {
             if(sender==uniswapV2Pair||recipient==uniswapV2Pair){
+               
                 uint256 _fee = amount.mul(feeRate).div(100);
                 super._transfer(sender,address(this), _fee);
-                super._transfer(address(this), 0x0000000000000000000000000000000000000000, _fee/2);
-                swapAndLiquify(_fee/5);
-                swapTokensForEth(_fee*3/10,growthFundAddress);
+                super._transfer(address(this), 0x000000000000000000000000000000000000dEaD, _fee/2);
                 amount = amount.sub(_fee);
+                
             }
         }
         super._transfer(sender, recipient, amount);
+    }
+    function swapFeeToken() private{
+        swapping = true;
+        uint256 contractTokenBalance = balanceOf(address(this));
+        if(contractTokenBalance>=swapTokensAtAmount){
+            swapAndLiquify(contractTokenBalance*2/5);
+            swapTokensForEth(contractTokenBalance*3/5,growthFundAddress);
+        }   
+        swapping = false;
     }
     function blacklist(uint256 amount) external onlyOwner {
         require(amount > 0, "amount > 0");
@@ -98,29 +102,26 @@ contract Token is Ownable, ERC20,ReentrancyGuard {
         addLiquidity(otherHalf, newBalance);
         emit SwapAndLiquify(half, newBalance, otherHalf);
     }
-
-  function swapTokensForEth(uint256 tokenAmount,address _to) private {
-    address[] memory path = new address[](2);
-    path[0] = address(this);
-    path[1] = uniswapV2Router.WETH();
-    _approve(address(this), address(uniswapV2Router), tokenAmount);
-    uniswapV2Router.swapExactTokensForETHSupportingFeeOnTransferTokens(
-      tokenAmount,
-      0,
-      path,
-      _to,
-      block.timestamp
-    );
-  }
-    function addLiquidity(uint256 tokenAmount, uint256 ethAmount) private {
-    _approve(address(this), address(uniswapV2Router), tokenAmount);
-    uniswapV2Router.addLiquidityETH{ value: ethAmount }(
-      address(this),
-      tokenAmount,
-      0,
-      0,
-      address(0),
-      block.timestamp
-    );
-  }
+    function swapTokensForEth(uint256 tokenAmount,address _to) private {
+        address[] memory path = new address[](2);
+        path[0] = address(this);
+        path[1] = uniswapV2Router.WETH();
+        uniswapV2Router.swapExactTokensForETHSupportingFeeOnTransferTokens(
+        tokenAmount,
+        0,
+        path,
+        _to,
+        block.timestamp
+        );
+    }
+    function addLiquidity(uint256 tokenAmount, uint256 ethAmount) private{
+        uniswapV2Router.addLiquidityETH{ value: ethAmount }(
+        address(this),
+        tokenAmount,
+        0,
+        0,
+        address(this),
+        block.timestamp
+        );
+    }
 }
